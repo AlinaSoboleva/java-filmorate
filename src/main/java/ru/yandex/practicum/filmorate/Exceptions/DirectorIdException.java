a46b4cf7ce2f433132af7cf341715fcf6dc5159a
Sergey Novikov <NovikS88@yandex.ru>
2023-06-22 23:38:32 +1000

    fix: delete DirectorIdException

--- src/main/java/ru/yandex/practicum/filmorate/Exceptions/DirectorIdException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.Exceptions;
-
-public class DirectorIdException extends RuntimeException{
-    public DirectorIdException(String message) {
-        super(message);
-    }
-}

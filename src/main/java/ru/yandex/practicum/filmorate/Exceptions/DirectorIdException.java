0b60462c61dc9b653e6435dbd83f7d75922704e4
Sergey Novikov <NovikS88@yandex.ru>
2023-06-22 23:13:03 +1000

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/Exceptions/DirectorIdException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exceptions/DirectorIdException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.Exceptions;
 
-public class DirectorIdException extends RuntimeException {
+public class DirectorIdException extends RuntimeException{
     public DirectorIdException(String message) {
         super(message);
     }

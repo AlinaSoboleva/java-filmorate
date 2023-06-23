68c67a3b080a4cdfa32da9824b0b086ce69a5525
Kir <you_can_more@mail.ru>
2023-06-24 00:21:40 +0200

    add director finished4

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/impl/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/impl/InMemoryFilmStorage.java
@@ -8,7 +8,7 @@ import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import java.util.*;
 import java.util.stream.Collectors;
 
-/*@Component("inMemoryFilmStorage")
+@Component("inMemoryFilmStorage")
 public class InMemoryFilmStorage implements FilmStorage {
 
     private final Map<Integer, Film> films = new HashMap<>();
@@ -87,4 +87,4 @@ public class InMemoryFilmStorage implements FilmStorage {
             return o2.getRate() - o1.getRate();
         }
     }
-}*/
+}
